dd4ed5ce894e457d9db5f63e9da231cacbec5f73
Anton Alekseevich <antonboch12.44@gmail.com>
2017-05-20 14:37:30 +0400

    Maked more readable code.
    And fix for non-CDATA description.
    And fix any &lt; &gt; in description.

--- pageGen.py
+++ pageGen.py
@@ -38,20 +38,61 @@ RSS_DATA = input_data.read()
 ch = re.findall(r".*?<channel>(?P<channel>.*?)</channel>.*", RSS_DATA, re.DOTALL)[0]
 _title = re.findall(r".*?<title>(?P<title>.*?)</title>(?P<aftT>.*?)", ch, re.DOTALL)
 RSS_title = _title[0][0]
-resgener = "<!DOCTYPE html><html><head><title>" + RSS_title + "</title><style>body { margin: 0; background: #AAA; } .news_card { margin: 50px 25px; border: 1px; border-radius: 3px; border-style: solid; border-color: rgba(127, 127, 127, 0.75); background-color: #dfdfdf; box-shadow: 0px 0px 20px 10px rgba(128, 128, 128, 40); } .news-headline { font-family: monospace; font-size: 2.5em; padding-left: 20px; width: 80%; } .news-textline { font-family: monospace;font-size: 1.5em; padding-left: 20px; width: 80%; } img { margin-right: 60px; width: 100%; }</style><meta charset=\"" + XML_ENCODING + "\"/></head><body>"
+resgener = "<!DOCTYPE html>" \
+           "<html>" \
+           "<head>" \
+           "<title>" + RSS_title + "</title>" \
+           "<style>" \
+               "body { " \
+                   "margin: 0; " \
+                   "background: #AAA; " \
+               "} " \
+               ".news_card { " \
+                   "margin: 50px 25px; " \
+                   "border: 1px; " \
+                   "border-radius: 3px; " \
+                   "border-style: solid; " \
+                   "border-color: rgba(127, 127, 127, 0.75); " \
+                   "background-color: #dfdfdf; " \
+                   "box-shadow: 0px 0px 20px 10px rgba(128, 128, 128, 40); " \
+               "} " \
+               ".news-headline { " \
+                   "font-family: monospace; " \
+                   "font-size: 3em; " \
+                   "padding-left: 20px; " \
+               "} " \
+               ".news-textline { " \
+                   "font-family: monospace; " \
+                   "font-size: 2.5em; " \
+                   "padding-left: 20px; " \
+               "} " \
+               "img { " \
+                   "height: calc(100% - 80px); " \
+                   "width: calc(100% - 20px); " \
+                   "box-shadow: 0px 0px 8px 2px rgba(128, 128, 128, 40); " \
+               "}" \
+           "</style>" \
+           "<meta charset=\"" + XML_ENCODING + "\"/>" \
+           "</head>" \
+           "<body>"
 _items = re.findall(r"<item>(?P<title>.*?)</item>", ch, re.DOTALL)
 for item in _items:
 	rss_title = re.findall(r".*<title>(?P<this>.*?)</title>(?P<secondLy>.*)", item, re.DOTALL)
 	rss_link = re.findall(r".*<link>(?P<this>.*?)</link>(?P<secondLy>.*)", rss_title[0][1], re.DOTALL)
 	resgener += "<div class=\"news_card\"><table><tr><td><table><tr><td class=\"news-headline\"><a href=\"" + rss_link[0][0] + "\">" + rss_title[0][0] + "</a></td></tr><tr><td class=\"news-textline\">"
 	rss_description = re.findall(r".*<description>(?P<this>.*?)</description>(?P<secondLy>.*)", rss_link[0][1], re.DOTALL)
-	normalize = re.findall(r"<!\[CDATA\[(?P<CDATA>.*?)\]\]>", rss_description[0][0], re.DOTALL)[0]
+	try:
+		normalize = re.findall(r"<!\[CDATA\[(?P<CDATA>.*?)\]\]>", rss_description[0][0], re.DOTALL)[0]
+	except:
+		normalize = rss_description[0][0]
+	for nop in range(0, 255, 1):
+		normalize = re.sub("&lt;", "<", normalize, re.DOTALL)
+		normalize = re.sub("&gt;", ">", normalize, re.DOTALL)
 	resgener += normalize + "</td></tr></table></td>"
 	try:
 		rss_enclosure_url = re.findall(r"<enclosure url=\"(?P<url>.*?) .*?/>", rss_description[0][1], re.DOTALL)
-		resgener += "<td><img src=\"" + rss_enclosure_url[0] + "/></td>"
+		resgener += "<td style=\" width:40%; \"><img src=\"" + rss_enclosure_url[0] + "\"/></td>"
 	except:
-		
 		pass
 	resgener += "</tr></table></div>"
 resgener += "</body></html>"
