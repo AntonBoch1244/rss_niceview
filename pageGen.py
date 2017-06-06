c8a635f60189d403ed3aaa4c43de7f508e62e12b
Anton Alekseevich <antonboch12.44@gmail.com>
2017-06-06 04:47:14 +0400

    Added ATOM Compatibility and turn off dark mode by default. if you wanted you can change style by adding filter to body.

--- pageGen.py
+++ pageGen.py
@@ -41,8 +41,16 @@ RSS_DATA = input_data.read()
 def CDATAUnpack(some_data):
 	return re.findall(r"<!\[CDATA\[(?P<CDATA>.*?)\]\]>", some_data, re.DOTALL)[0]
 
-
-ch = re.findall(r".*?<channel.*?>(?P<channel>.*?)</channel>.*", RSS_DATA, re.DOTALL)[0]
+try:
+	ch = re.findall(r".*?<channel.*?>(?P<channel>.*?)</channel>.*", RSS_DATA, re.DOTALL)[0]
+	print("RSS Detected!")
+	RSS = True
+	ATOM = False
+except IndexError:
+	ch = re.findall(r".*?<feed.*?>(?P<channel>.*?)</feed>.*", RSS_DATA, re.DOTALL)[0]
+	print("ATOM Detected!")
+	RSS = False
+	ATOM = True
 _title = re.findall(r".*?<title>(?P<title>.*?)</title>(?P<aftT>.*?)", ch, re.DOTALL)
 RSS_title = _title[0][0]
 resgener = "<!DOCTYPE html>"\
@@ -52,8 +60,7 @@ resgener = "<!DOCTYPE html>"\
                                    "<style>"\
                                    "body { "\
                                    "margin: 0; "\
-                                   "background: #AAA; "\
-                                   "filter: sepia(100%) invert(100%); "\
+                                   "background: #AAAAAA; "\
                                    "} "\
                                    ".news_card { "\
                                    "margin: 50px 25px; "\
@@ -65,14 +72,16 @@ resgener = "<!DOCTYPE html>"\
                                    "box-shadow: 0px 0px 20px 10px rgba(128, 128, 128, 40); "\
                                    "} "\
                                    ".news-headline { "\
-                                   "font-family: monospace; "\
-                                   "font-size: 3em; "\
+                                   "font-family: serif; "\
+                                   "font-size: 2em; "\
                                    "padding-left: 20px; "\
+                                   "text-align: justify;"\
                                    "} "\
                                    ".news-textline { "\
-                                   "font-family: monospace; "\
-                                   "font-size: 2.5em; "\
+                                   "font-family: serif; "\
+                                   "font-size: 1.5em; "\
                                    "padding-left: 20px; "\
+                                   "text-align: justify;"\
                                    "} "\
                                    "img { "\
                                    "height: calc(100% - 80px); "\
@@ -83,46 +92,109 @@ resgener = "<!DOCTYPE html>"\
                                    "<meta charset=\"" + XML_ENCODING + "\"/>"\
                                                                        "</head>"\
                                                                        "<body>"
-_items = re.findall(r"<item>(?P<title>.*?)</item>", ch, re.DOTALL)
-for item in _items:
-	rss_title = re.findall(r".*<title>(?P<this>.*?)</title>.*", item, re.DOTALL)
-	try:
-		ttl = CDATAUnpack(rss_title[0])
-	except:
-		ttl = rss_title[0]
-	rss_link = re.findall(r".*<link>(?P<this>.*?)</link>.*", item, re.DOTALL)
-	resgener +=\
-		"<div class=\"news_card\">"\
-		"<table>"\
-		"<tr>"\
-		"<td>"\
-		"<table>"\
-		"<tr>"\
-		"<td class=\"news-headline\">"\
-		"<a href=\"" + rss_link[0] + "\">" + ttl + "</a>"\
-		                                           "</td>"\
-		                                           "</tr>"\
-		                                           "<tr>"\
-		                                           "<td class=\"news-textline\">"
-	rss_description = re.findall(r".*<description>(?P<this>.*?)</description>.*",
-	                             item,
-	                             re.DOTALL)
-	try:
-		normalize = CDATAUnpack(rss_description[0])
-	except:
-		normalize = rss_description[0]
-	for nop in range(0, 255, 1):
-		normalize = re.sub("&lt;", "<", normalize, re.DOTALL)
-		normalize = re.sub("&gt;", ">", normalize, re.DOTALL)
-	resgener += normalize + "</td></tr></table></td>"
-	try:
-		rss_enclosure_url = re.findall(r"<enclosure url=\"(?P<url>.*?) .*?/>", item, re.DOTALL)
-		resgener += "<td style=\" width:40%; \"><img src=\"" + rss_enclosure_url[0] + "\"/></td>"
-	except:
-		pass
-	resgener += "</tr>"\
-	            "</table>"\
-	            "</div>"
+if RSS:
+	_items = re.findall(r"<item>(?P<title>.*?)</item>", ch, re.DOTALL)
+	_Entrys = None
+if ATOM:
+	_items = None
+	_Entrys = re.findall(r"<entry>(?P<title>.*?)</entry>", ch, re.DOTALL)
+
+try:
+	for item in _items:
+		rss_title = re.findall(r".*<title>(?P<this>.*?)</title>.*", item, re.DOTALL)
+		try:
+			ttl = CDATAUnpack(rss_title[0])
+		except:
+			ttl = rss_title[0]
+		rss_link = re.findall(r".*<link>(?P<this>.*?)</link>.*", item, re.DOTALL)
+		resgener +=\
+			"<div class=\"news_card\">"\
+			"<table>"\
+			"<tr>"\
+			"<td>"\
+			"<table>"\
+			"<tr>"\
+			"<td class=\"news-headline\">"\
+			"<a href=\"" + rss_link[0] + "\">" + ttl + "</a>"\
+			                                           "</td>"\
+			                                           "</tr>"\
+			                                           "<tr>"\
+			                                           "<td class=\"news-textline\">"
+		rss_description = re.findall(r".*<description>(?P<this>.*?)</description>.*",
+		                             item,
+		                             re.DOTALL)
+		try:
+			normalize = CDATAUnpack(rss_description[0])
+		except:
+			normalize = rss_description[0]
+		for nop in range(0, 255, 1): normalize = re.sub("&lt;", "<", normalize, re.DOTALL)
+		for nop in range(0, 255, 1): normalize = re.sub("&gt;", ">", normalize, re.DOTALL)
+		for nop in range(0, 255, 1): normalize = re.sub("&#39;", "'", normalize, re.DOTALL)
+		for nop in range(0, 255, 1): normalize = re.sub("&nbsp;", "", normalize, re.DOTALL)
+		for nop in range(0, 255, 1): normalize = re.sub("&quot;", "\"", normalize, re.DOTALL)
+		for nop in range(0, 255, 1): normalize = re.sub("&amp;", "&", normalize, re.DOTALL)
+		resgener += normalize + "</td></tr></table></td>"
+		try:
+			rss_enclosure_url = re.findall(r"<enclosure url=\"(?P<url>.*?)\" .*?/>", item, re.DOTALL)
+			resgener += "<td style=\" width:40%; \"><a href=\"" + rss_enclosure_url[0] + "\"><img src=\"" + rss_enclosure_url[0] + "\"/></a></td>"
+		except:
+			pass
+		resgener += "</tr>"\
+		            "</table>"\
+		            "</div>"
+except:
+	if RSS:
+		print("No Items")
+	pass
+
+try:
+	for entry in _Entrys:
+		ATOM_title = re.findall(r".*<title>(?P<this>.*?)</title>.*", entry, re.DOTALL)
+		try:
+			ttl = CDATAUnpack(ATOM_title[0])
+		except:
+			ttl = ATOM_title[0]
+		rss_link = re.findall(r".*<link .*? href=\"(?P<this>.*?)\".*/>.*", entry, re.DOTALL)
+		resgener +=\
+			"<div class=\"news_card\">"\
+			"<table>"\
+			"<tr>"\
+			"<td>"\
+			"<table>"\
+			"<tr>"\
+			"<td class=\"news-headline\">"\
+			"<a href=\"" + rss_link[0] + "\">" + ttl + "</a>"\
+			                                           "</td>"\
+			                                           "</tr>"\
+			                                           "<tr>"\
+			                                           "<td class=\"news-textline\">"
+		rss_description = re.findall(r".*<summary.*?>(?P<this>.*?)</summary>.*",
+		                             entry,
+		                             re.DOTALL)
+		try:
+			normalize = CDATAUnpack(rss_description[0])
+		except:
+			normalize = rss_description[0]
+		for nop in range(0, 255, 1): normalize = re.sub("&lt;", "<", normalize, re.DOTALL)
+		for nop in range(0, 255, 1): normalize = re.sub("&gt;", ">", normalize, re.DOTALL)
+		for nop in range(0, 255, 1): normalize = re.sub("&#39;", "'", normalize, re.DOTALL)
+		for nop in range(0, 255, 1): normalize = re.sub("&nbsp;", "", normalize, re.DOTALL)
+		for nop in range(0, 255, 1): normalize = re.sub("&quot;", "\"", normalize, re.DOTALL)
+		for nop in range(0, 255, 1): normalize = re.sub("&amp;", "&", normalize, re.DOTALL)
+		resgener += normalize + "</td></tr></table></td>"
+		try:
+			rss_enclosure_url = re.findall(r"<image>(?P<url>.*?)</image>", entry, re.DOTALL)
+			resgener += "<td style=\" width:40%; \"><img src=\"" + rss_enclosure_url[0] + "\"/></td>"
+		except:
+			pass
+		resgener += "</tr>"\
+		            "</table>"\
+		            "</div>"
+except Exception as x:
+	if ATOM:
+		print("No Entrys, {}".format(x))
+	pass
+
 resgener += "</body>"\
             "</html>"
 
